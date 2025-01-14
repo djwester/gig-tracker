edd90142274cf10b172baf316831bbfe34340517
Dave Westerveld <davewesterveld@gmail.com>
2025-01-14 07:42:41 -0500

    add requests argument as required by slowapi

--- gigtracker/routes/gig_routes.py
+++ gigtracker/routes/gig_routes.py
@@ -13,7 +13,7 @@ gig_router = APIRouter(prefix="/api")
 
 @gig_router.post("/gigs", response_model=GigPublic, status_code=201)
 @limiter.limit("5/minute")
-def create_gig(gig: GigCreate, session: SessionDep):
+def create_gig(request: Request, gig: GigCreate, session: SessionDep):
     db_gig = Gig.model_validate(gig)
 
     session.add(db_gig)
@@ -33,7 +33,7 @@ def get_gigs(request: Request, session: SessionDep):
 
 @gig_router.put("/gigs/{gig_id}", response_model=GigPublic)
 @limiter.limit("25/minute")
-def update_gig(gig_id: int, gig: GigCreate, session: SessionDep):
+def update_gig(request: Request, gig_id: int, gig: GigCreate, session: SessionDep):
     db_gig = session.get(Gig, gig_id)
     if not db_gig:
         raise HTTPException(status_code=404, detail="Gig not found")
@@ -49,7 +49,7 @@ def update_gig(gig_id: int, gig: GigCreate, session: SessionDep):
 
 @gig_router.delete("/gigs/{gig_id}", status_code=204)
 @limiter.limit("10/minute")
-def delete_gig(gig_id: int, session: SessionDep):
+def delete_gig(request: Request, gig_id: int, session: SessionDep):
     gig = session.get(Gig, gig_id)
     if not gig:
         raise HTTPException(status_code=404, detail="Gig not found")
